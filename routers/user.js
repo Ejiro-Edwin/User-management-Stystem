1587c18f2013fad4ccc5772f6a54bb1dcb93af68
Leelsmuth <femianjorin@rocketmail.com>
2020-02-02 12:48:10 +0100

    created register and login endpoints

---
+++ routers/user.js
@@ -0,0 +1,33 @@
+const router = require("express").Router();
+const User = require('../models/user');
+
+
+router.post('/', async (req, res) => {
+    // Create a new user
+    try {
+        const user = new User(req.body)
+        await user.save()
+        const token = await user.generateAuthToken()
+        res.status(201).send({ user, token })
+    } catch (error) {
+        res.status(400).send(error.message)
+    }
+})
+
+router.post('/login', async (req, res) => {
+    //Login a registered user
+    try {
+        const { email, password } = req.body
+        const user = await User.findByCredentials(email, password)
+        if (!user) {
+            return res.status(401).send({ error: 'Login failed! Check authentication credentials' })
+        }
+        const token = await user.generateAuthToken()
+        res.send({ user, token })
+    } catch (error) {
+        res.status(400).send(error.message)
+    }
+
+})
+
+module.exports = router
